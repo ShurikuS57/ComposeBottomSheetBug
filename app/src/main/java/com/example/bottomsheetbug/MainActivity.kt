1d17351e93e355a787917377c64c1acd02ddbe4a
ShurikuS57 <cawa007@gmail.com>
2022-10-19 21:17:25 +0300

    First commit

---
+++ app/src/main/java/com/example/bottomsheetbug/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.bottomsheetbug
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bottomsheetbug.ui.theme.BottomSheetBugTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
+    @OptIn(ExperimentalMaterialApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+            )
+            val coroutineScope = rememberCoroutineScope()
+
+            BottomSheetBugTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    BottomSheetScaffold(
+                        scaffoldState = bottomSheetScaffoldState,
+                        sheetContent = {
+                            Box(
+                            ) {
+                                Content()
+                            }
+                        }, sheetPeekHeight = 0.dp
+                    ) {
+                        Button(onClick = {
+                            coroutineScope.launch {
+                                if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                                    bottomSheetScaffoldState.bottomSheetState.expand()
+                                } else {
+                                    bottomSheetScaffoldState.bottomSheetState.collapse()
+                                }
+                            }
+                        }) {
+                            Text(text = "Expand/Collapse Bottom Sheet")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Content() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .background(Color.Blue)
+    ) {
+        Spacer(modifier = Modifier.height(150.dp))
+        TextField(value = "", colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Red), onValueChange = {})
+        Spacer(modifier = Modifier.height(40.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text("Click")
+        }
+        Spacer(modifier = Modifier.height(140.dp))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BottomSheetBugTheme {
+        Content()
+    }
+}
